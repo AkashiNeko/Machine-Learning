4ba6436ca61bd393e588bcb8072d8d930e5db858
AkashiNeko <akashineko@qq.com>
2023-05-13 16:16:47 +0800

    2023.5.13

---
+++ cnn/my_conv_net.py
@@ -0,0 +1,27 @@
+import torch
+import torch.nn as nn
+
+
+class ConvNet(nn.Module):
+    def __init__(self, hidden_size=128, output_size=10):
+        super(ConvNet, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(1, 16, kernel_size=5, stride=1, padding=2),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(16, 32, kernel_size=5, stride=1, padding=2),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.fc1 = nn.Linear(7 * 7 * 32, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = out.reshape(out.size(0), -1)
+        out = self.fc1(out)
+        out = self.fc2(out)
+        return out
---
+++ cnn/train.py
@@ -0,0 +1,49 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+from my_conv_net import *
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model_path = 'cnn.pth'
+mnist_path = './data'
+
+# 数据预处理
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.1307,), (0.3081,))
+])
+
+# 加载MNIST数据集
+train_dataset = datasets.MNIST(root=mnist_path, train=True, transform=transform, download=True)
+
+# 定义数据加载器
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=100, shuffle=True, pin_memory=True)
+
+
+# 定义模型、损失函数和优化器
+model = ConvNet().to(device)
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+# 训练模型
+epochs = 5
+for epoch in range(epochs):
+    for i, (images, labels) in enumerate(train_loader):
+        images = images.to(device)
+        labels = labels.to(device)
+        # 前向传播和计算损失函数
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+
+        # 反向传播和优化
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if (i + 1) % 100 == 0:
+            print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'.format(epoch + 1, epochs, i + 1, len(train_loader), loss.item()))
+
+# 保存模型
+torch.save(model.state_dict(), model_path)
